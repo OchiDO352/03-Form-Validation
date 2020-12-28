ccede7c7f019bb65ffc9d251dba683714e04d541
OchiDO352 <75597102+OchiDO352@users.noreply.github.com>
2020-12-28 22:24:28 +0700

    Update script.js

--- 03-Form Validation/script.js
+++ 03-Form Validation/script.js
@@ -1,22 +1,3 @@
-//! Dark light mode
-const options = {
-    bottom: '32px', // default: '32px'
-    right: '32px', // default: '32px'
-    left: 'unset', // default: 'unset'
-    time: '0.3s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#fff', // default: '#fff'
-    saveInCookies: true, // default: true,
-    label: '🌗', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
-  
-  const darkmode = new Darkmode(options);
-  darkmode.showWidget();
-
-
 //! ture false mode  
   // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function () {
